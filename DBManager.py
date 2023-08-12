4d53061816010cb611a36aca4df7ed0e6daae775
dmitry <dima.trenirovki@mail.ru>
2023-08-12 08:44:55 +0300

    course_work

---
+++ DBManager.py
@@ -0,0 +1,75 @@
+import psycopg2
+
+
+class DBManager:
+    def __init__(self):
+        self.conn = psycopg2.connect(host='localhost',
+                                     database='vacancy',
+                                     user='postgres',
+                                     password='qwerty')
+
+    def safe_vacancy(self, vacancies: list[dict]):
+        employeers = []
+        with self.conn.cursor() as cursor:
+            for vacancy in vacancies:
+                cursor.execute(
+                    f'insert into vacancies(description, employer, experience, salary, url)'
+                    f'values (%s, %s, %s, %s, %s)',
+                    (vacancy.get('description'), vacancy['employeer'].lower(), vacancy['experience'], vacancy['salary'],
+                     vacancy['url']))
+                if vacancy['employeer'] not in employeers:
+                    employeers.append(vacancy['employeer'])
+        self.conn.commit()
+        self.safe_employeers(employeers)  # Связка двух методов
+
+    def safe_employeers(self, employeers: list[str]):
+        with self.conn.cursor() as cursor:
+            for employer in employeers:
+                cursor.execute('select id from employers where company_name = %s', [employer.lower()])
+                if not cursor.fetchall():
+                    cursor.execute('insert into employers(company_name) values(%s)', [employer])
+            self.conn.commit()
+
+    def get_companies_and_vacancies_count(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute('select count(id), employer from vacancies group by employer')
+            #print(cursor.fetchall())
+            for company_name, count in cursor.fetchall():
+                print(f"company: {count}, count: {company_name}")
+
+    def get_all_vacancies(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute('select * from vacancies')
+            for vacancy in cursor.fetchall():
+                print(f"discription: {vacancy[1]}, employeer: {vacancy[2]}, experience: {vacancy[3]},"
+                      f" salary: {vacancy[4]}, url: {vacancy[-1]}")  #4 Первая строка при выводе
+
+
+    def get_avg_salary(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute('select avg(salary) from vacancies')
+            print(round(cursor.fetchall()[0][0]))
+
+    def get_vacancies_with_higher_salary(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute(f'select * from vacancies where salary > {self.get_avg_salary()}')
+            for vacancy in cursor.fetchall():
+                print(vacancy[1])
+            #print(cursor.fetchall())
+            #print(self.get_avg_salary()
+
+
+    def get_vacancies_with_keyword(self, *keyword: str):
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"select * from vacancies where position(%s in description)>0", keyword)
+            for vacancy in cursor.fetchall():
+                print((vacancy[1]))
+
+
+manager = DBManager()
+#manager.safe_vacancy()
+#print(manager.get_all_vacancies())
+#print(manager.get_companies_and_vacancies_count())
+print(manager.get_vacancies_with_keyword('Спец'))
+#print(manager.get_avg_salary())
+
---
+++ hh.py
@@ -0,0 +1,65 @@
+import requests
+from pprint import pprint
+
+
+class Pars:
+
+    def get_vacansions(self, name_companies: list[str], key_word: list[str]):
+        lst = []
+        words = '+'.join(key_word)
+        employeers_id = self.get_employeers(name_companies)
+        for i in employeers_id:
+            URL = f'https://api.hh.ru/vacancies?per_page=15&employer_id={i["id"]}&text={words}'
+            responce = requests.get(URL).json()
+            if responce['items']:
+                pprint(responce['items'])
+                #for j in responce['items']:
+                    #lst.append(self.pars_vanshions(j, i['company']))  # 1Что мы добавляем и из какого метода и почему?
+        #return lst
+
+    def get_employeers(self, name_companies: list[str]):
+        employeer_id = []
+        for name in name_companies:
+            URL = f'https://api.hh.ru/employers?text={name}&only_with_vacancies=True'  #2как были найдены слова в фигурных скобках и есть ли взаимосвязь с 22 строкой
+            responce = requests.get(URL).json()
+            # print(responce)
+            for id_company in responce['items']:
+                employeer_id.append({'company': name, 'id': id_company['id']})
+        return employeer_id
+
+    def pars_vanshions(self, vacansi: dict, company: str):
+        employeer = company
+        description = vacansi['name']  # it is j
+        url = vacansi['alternate_url'] # j
+        salary = self.get_salary(vacansi['salary']) #j
+        experience = vacansi['experience']['name']
+        vacanci = {'employeer': employeer,
+                   'description': description, 'url': url,
+                   'experience': experience, 'salary': salary}
+        return vacanci
+
+    def get_salary(self, salary: dict | None):
+        if salary:
+            if salary['from'] and salary['to']:
+                return (salary['from'] + salary['to']) // 2
+            elif salary['from']:
+                return salary['from']
+            elif salary['to']:
+                return salary['to']
+        else:
+            return 0
+
+
+# URL = 'https://api.hh.ru/vacancies'
+# responce = requests.get(URL).json()
+# employer = []
+# print(responce)
+# for i in responce['items']:
+#     employer.append(i['premium'])
+#     #print(i['salary'])
+#     print(employer)
+
+
+pars = Pars()
+#pprint(pars.get_vacansions(['МТС'], []))
+# print(pars.get_employeers(['ozon', 'avito']))
